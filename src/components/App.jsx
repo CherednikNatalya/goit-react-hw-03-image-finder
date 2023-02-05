88a25704f407cb5e7c16fbb56637e228eddf9e76
CherednikNatalya <104647370+CherednikNatalya@users.noreply.github.com>
2023-02-05 17:37:29 +0200

    789

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,96 @@
 import { Component } from "react";
 import {Searchbar} from './Searchbar/Searchbar'
 import {ImageGallery} from './ImageGallery/ImageGallery'
+import { Button } from './Button/Button';
+import { Loader } from './Loader/Loader';
+import { fetchPichureData } from '../services/service';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import Notiflix from 'notiflix';
+
+
+const STATUS = {
+  idle: 'idle',
+  pending: 'pending',
+  success: 'success',
+  rejected: 'rejected',
+};
 
 
 export  class App extends Component {
   state = {
     searchQuery: '',
+    imgData: [],
+    page: 1,
+    status: STATUS.idle,
   }
-  
+
+
+
+  componentDidUpdate(_, prevState) {
+
+    const { searchQuery, page } = this.state;
+    const { page: prevPage, searchQuery: prevQuery } = prevState;
+
+    if (searchQuery !== prevQuery || page !== prevPage) {
+      this.fetchData();
+     
+    }
+  }
+
+  fetchData = async () => {
+    this.setState({ status: STATUS.pending });
+    const { searchQuery, page } = this.state;
+    
+    try {
+      const requestData = await fetchPichureData(searchQuery, page);
+      if (requestData.data.total === 0) {
+        this.setState({
+          status: STATUS.rejected,
+        });
+        Notiflix.Notify.failure('No images');
+        return;
+      }
+      this.setState(({ imgData: prevData }) => ({
+        imgData: [...prevData, ...requestData.data.hits],
+        status: STATUS.success,
+      }));
+    } catch (error) {
+      this.setState({
+        status: STATUS.rejected,
+      });
+      Notiflix.Notify.failure('No images');
+    }
+  };
+
+
+  loadMore = () => {
+    this.setState(({ page: prevPage }) => ({ page: prevPage + 1 }));
+  };
+
 
 onSubmit = searchQuery => {
-this.setState ({searchQuery})
+this.setState ({
+  searchQuery,
+  imgData: [],
+  page: 1,})
 }
 
 render () {
-const {searchQuery} = this.state;
+const {imgData, status} = this.state;
 
 
   return (
     <>
       <Searchbar onSubmit={this.onSubmit}/>
-      <ImageGallery searchQuery={searchQuery}/>
+      <ImageGallery imgData= {imgData}/>
+
+      {status === STATUS.pending && <Loader />}
+
+      {status === STATUS.success && !(imgData.length < 12)&&
+            (<Button onClick={this.loadMore} />
+          )}
+
       <ToastContainer
           position="top-right"
           autoClose={1500}
@@ -41,6 +109,7 @@ const {searchQuery} = this.state;
   }
 
 
+ 
 
 
 
@@ -48,35 +117,32 @@ const {searchQuery} = this.state;
 
 
 
-  
-
-
-//   import { Component } from 'react';
-// import { Searchbar } from './Searchbar/Searchbar';
-// import { ImageGallery } from './ImageGallery/ImageGallery';
+//   import { Component } from "react";
+// import {Searchbar} from './Searchbar/Searchbar'
+// import {ImageGallery} from './ImageGallery/ImageGallery'
 // import { ToastContainer } from 'react-toastify';
 // import 'react-toastify/dist/ReactToastify.css';
 
-// //import data from './assets/data.json';
 
-// export class App extends Component {
+// export  class App extends Component {
 //   state = {
 //     searchQuery: '',
-//   };
+//   }
+  
+
+// onSubmit = searchQuery => {
+// this.setState ({searchQuery})
+// }
+
+// render () {
+// const {searchQuery} = this.state;
 
 
-//   formSubmit = searchQuery => {
-//     this.setState({ searchQuery });
-//   };
-
-//   render() {
-//     const { searchQuery, } = this.state;
-
-//     return (
-//       <>
-//         <Searchbar onSubmit={this.formSubmit} />
-//         <ImageGallery searchQuery={searchQuery} />
-//         <ToastContainer
+//   return (
+//     <>
+//       <Searchbar onSubmit={this.onSubmit}/>
+//       <ImageGallery searchQuery={searchQuery}/>
+//       <ToastContainer
 //           position="top-right"
 //           autoClose={1500}
 //           hideProgressBar={false}
@@ -88,7 +154,142 @@ const {searchQuery} = this.state;
 //           pauseOnHover
 //           theme="colored"
 //         />
+//     </>
+//   )
+// }  
+//   }
+
+
+
+
+
+// import { Component } from "react";
+// import fetchImages from "../API/Api";
+// import { Searchbar } from "../Searchbar/Searchbar";
+// import { Loader } from "../Loader/Loader";
+// import { ToastContainer, toast } from "react-toastify";
+// import 'react-toastify/dist/ReactToastify.css';
+// import { ImageGallery } from "../ImageGallery/ImageGallery";
+// import { Button } from "../Button/Button";
+// import Modal from "../Modal/Modal";
+
+
+// export class App extends Component {
+//   state = {
+//     searchData: '',
+//     images: [],
+//     page: 1,
+//     largeImage: '',
+//     showModal: false,
+//     isLoading: false,
+//     error: null,
+//     totalResults: [],
+//     showButton: false,
+//   };
+
+//   componentDidUpdate(_, prevState) {
+//     const prevPage = prevState.page;
+//     const prevSearchData = prevState.searchData;
+//     const { searchData, page } = this.state;
+
+//     if (prevPage !== page || prevSearchData !== searchData) {
+//       try {
+//         this.setState({ isLoading: true, showButton: false });
+//         const response = fetchImages(searchData, page);
+
+//         response.then(data => {
+//           if (data.data.hits.length === 0) {
+//             toast.error('Nothing was found');
+//           }
+
+//           this.setState(({ images }) => ({
+//             images: [...images, ...data.data.hits]
+//           }));
+
+//           this.setState({
+//             isLoading: false,
+//             showButton:
+//               this.state.page < Math.ceil(data.data.totalHits / 12)
+//                 ? true
+//                 : false
+//           });
+//         });
+//       } catch (error) {
+//         this.setState({ error, isLoading: false, showButton: false });
+//       }
+//     }
+//   }
+
+//   onSubmit = (searchData) => {
+//     if (searchData === '') {
+//       return toast.error('Write something');
+//     } else if (searchData === this.state.searchData) {
+//       return;
+//     }
+//     this.setState({
+//       searchData,
+//       images: [],
+//       page: 1,
+//     });
+//   };
+
+//   nextPage = () => {
+//     this.setState(({ page }) => ({ page: page + 1 }));
+//   };
+
+//   openModal = index => {
+//     this.setState(({ images }) => ({
+//       showModal: true,
+//       largeImage: images[index].largeImageURL
+//     }));
+//   };
+
+//   toggleModal = () => {
+//     this.setState(({ showModal }) => ({ showModal: !showModal }));
+//   };
+
+//   render() {
+//     const { onSubmit, nextPage, openModal, toggleModal } = this;
+//     const {
+//       images,
+//       largeImage,
+//       showModal,
+//       isLoading,
+//       showButton } = this.state;
+
+//     return (
+//       <>
+//         <Searchbar onSubmit={onSubmit} />
+//         {images.length !== 0 && (
+//           <ImageGallery images={images} openModal={openModal} />
+//         )}
+//         {showModal && (
+//           <Modal toggleModal={toggleModal} largeImage={largeImage} />
+//         )}
+//         {isLoading && <Loader />}
+//         <ToastContainer autoClose={2500} />
+//         {showButton && <Button nextPage={nextPage} />}
 //       </>
 //     );
-//   }
+//   };
 // }
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+  
+
+
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,77 +1,11 @@
 import css from './ImageGallery.module.css';
 
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import { Loader } from '../Loader/Loader';
+import { Section } from '../Section/Section';
 
-import { Section } from 'components/Section/Section';
-import { Button } from 'components/Button/Button';
-import { Component } from 'react';
-import { fetchPichureData } from '../../services/service';
-
-
-import Notiflix from 'notiflix';
-
-const STATUS = {
-  idle: 'idle',
-  pending: 'pending',
-  success: 'success',
-  rejected: 'rejected',
-};
-
-export class ImageGallery extends Component {
-  state = {
-    imgData: [],
-    page: 1,
-    per_page: 12,
-    status: STATUS.idle,
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    const { searchQuery } = this.props;
-    const { searchQuery: prevQuery } = prevProps;
-    const { page } = this.state;
-    const { page: prevPage } = prevState;
-
-    if (searchQuery !== prevQuery) {
-      this.setState({ page: 1, imgData: []});
-    }
-    if (searchQuery !== prevQuery || page !== prevPage) {
-      this.setState({ page: 1, imgData: []});
-      this.fetchData();
-    }
-  }
-
-  fetchData = async () => {
-    this.setState({ status: STATUS.pending });
-    const { searchQuery } = this.props;
-    const { page, per_page } = this.state;
-    try {
-      const requestData = await fetchPichureData(searchQuery, page, per_page);
-      if (requestData.data.total === 0) {
-        this.setState({
-          status: STATUS.rejected,
-        });
-        Notiflix.Notify.failure('No images');
-        return;
-      }
-      this.setState(({ imgData: prevData }) => ({
-        imgData: [...prevData, ...requestData.data.hits],
-        status: STATUS.success,
-      }));
-    } catch (error) {
-      this.setState({
-        status: STATUS.rejected,
-      });
-      Notiflix.Notify.failure('No images');
-    }
-  };
-
-  loadMore = () => {
-    this.setState(({ page: prevPage }) => ({ page: prevPage + 1 }));
-  };
-
-  render() {
-    const { status, imgData,per_page } = this.state;
+export const ImageGallery =({imgData}) => {
+ 
+  // const {imgData} = this.props;
 
     return (
       <Section className="gallery">
@@ -81,15 +15,108 @@ export class ImageGallery extends Component {
               <ImageGalleryItem key={imgItem.id} {...imgItem} />
             ))}
           </ul>
-          {status === STATUS.pending && <Loader />}
-          {status === STATUS.success && !(imgData.length<per_page)&&
-            (<Button onClick={this.loadMore} />
-          )}
         </>
       </Section>
     );
   }
-}
+
+
+
+
+
+
+// import css from './ImageGallery.module.css';
+
+// import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
+// import { Loader } from '../Loader/Loader';
+
+// import { Section } from 'components/Section/Section';
+// import { Button } from 'components/Button/Button';
+// import { Component } from 'react';
+// import { fetchPichureData } from '../../services/service';
+
+
+// import Notiflix from 'notiflix';
+
+// const STATUS = {
+//   idle: 'idle',
+//   pending: 'pending',
+//   success: 'success',
+//   rejected: 'rejected',
+// };
+
+// export class ImageGallery extends Component {
+//   state = {
+//     imgData: [],
+//     page: 1,
+//     per_page: 12,
+//     status: STATUS.idle,
+//   };
+
+//   componentDidUpdate(prevProps, prevState) {
+//     const { searchQuery } = this.props;
+//     const { searchQuery: prevQuery } = prevProps;
+//     const { page } = this.state;
+//     const { page: prevPage } = prevState;
+
+//     if (searchQuery !== prevQuery) {
+//       this.setState({ page: 1, imgData: []});
+//     }
+//     if (searchQuery !== prevQuery || page !== prevPage) {
+//       this.setState({ page: 1, imgData: []});
+//       this.fetchData();
+//     }
+//   }
+
+//   fetchData = async () => {
+//     this.setState({ status: STATUS.pending });
+//     const { searchQuery } = this.props;
+//     const { page, per_page } = this.state;
+//     try {
+//       const requestData = await fetchPichureData(searchQuery, page, per_page);
+//       if (requestData.data.total === 0) {
+//         this.setState({
+//           status: STATUS.rejected,
+//         });
+//         Notiflix.Notify.failure('No images');
+//         return;
+//       }
+//       this.setState(({ imgData: prevData }) => ({
+//         imgData: [...prevData, ...requestData.data.hits],
+//         status: STATUS.success,
+//       }));
+//     } catch (error) {
+//       this.setState({
+//         status: STATUS.rejected,
+//       });
+//       Notiflix.Notify.failure('No images');
+//     }
+//   };
+
+//   loadMore = () => {
+//     this.setState(({ page: prevPage }) => ({ page: prevPage + 1 }));
+//   };
+
+//   render() {
+//     const { status, imgData,per_page } = this.state;
+
+//     return (
+//       <Section className="gallery">
+//         <>
+//           <ul className={css.container}>
+//             {imgData.map(imgItem => (
+//               <ImageGalleryItem key={imgItem.id} {...imgItem} />
+//             ))}
+//           </ul>
+//           {status === STATUS.pending && <Loader />}
+//           {status === STATUS.success && !(imgData.length<per_page)&&
+//             (<Button onClick={this.loadMore} />
+//           )}
+//         </>
+//       </Section>
+//     );
+//   }
+// }
 
 
 
